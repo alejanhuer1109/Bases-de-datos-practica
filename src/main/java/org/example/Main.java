b9ebb0ab83f2cf27f73cc743340f3c8a31750456
alejanhuer1109 <paulamh2022@gmail.com>
2025-02-20 21:49:57 -0500

    Salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola clase");
+        System.out.println("Estamos aprendiendo comandos GIT");
+        System.out.println("Repasando comandos");
     }
 }
 
