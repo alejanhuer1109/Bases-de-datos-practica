98ca614a80c9512f9f5ab292b052a5aff56aed90
alejanhuer1109 <[paulamh2022@gmail.com]>
2025-02-26 22:39:46 -0500

    Archivo comandos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,22 @@ public class Main {
         System.out.println("Hola clase");
         System.out.println("Estamos aprendiendo comandos GIT");
         System.out.println("Repasando comandos");
+        System.out.println("Repasando comandos");
+        System.out.println("Repasando comandos");
+        System.out.println("Repasando comandos");
+        System.out.println("Repasando comandos");
+
+        System.out.println("SUMA");
+        System.out.println("RESTA");
+        System.out.println("MULTIPLICACION");
+        System.out.println("DIVISION");
+
+        System.out.println("DDL");
+        System.out.println("DML");
+        System.out.println("TCL");
+        System.out.println("Comandos Bases de Datos");
+        System.out.println("Comandos GIT");
     }
 }
+
 
